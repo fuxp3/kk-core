cf902b08a24978d680b81a06a214a27168ab9fc7
fuxp3 <fsmtp@sina.com>
2022-09-27 14:47:33 +0800

    feat: 删除run子命令
    删除run子命令

--- cmd/root.go
+++ cmd/root.go
@@ -5,25 +5,49 @@ Copyright © 2022 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
+	"kk-core/common"
+	"kk-core/core/connector"
+	"kk-core/core/module"
+	"kk-core/core/pipeline"
 	"os"
 
 	"github.com/spf13/cobra"
 )
 
 // rootCmd represents the base command when called without any subcommands
-var rootCmd = &cobra.Command{
-	Use:   "kk-core",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
+var (
+	name     string
+	command  string
+	parallel bool
+	retry    int
+	hosts    []connector.BaseHost
+	rootCmd  = &cobra.Command{
+		Use:   "run",
+		Short: "执行命令",
+		Long:  "远程主机执行命令",
+		// Uncomment the following line if your bare application
+		// has an action associated with it:
+		// Run: func(cmd *cobra.Command, args []string) { },
+		Run: func(cmd *cobra.Command, args []string) {
+			p := pipeline.Pipeline{
+				Name: "执行命令流水线",
+				Modules: []module.Module{
+					&common.RunModule{
+						Hostname: name,
+						Command:  command,
+						Parallel: parallel,
+						Retry:    retry,
+					},
+				},
+				Runtime: NewRuntime(),
+			}
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
-}
+			p.Start()
+		},
+	}
+)
 
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
@@ -43,5 +67,24 @@ func init() {
 
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
-	rootCmd.AddCommand(runCmd)
+	file, err := ioutil.ReadFile("./host.yaml")
+	if err != nil {
+		panic(any("未加载到主机配置文件"))
+	}
+	yaml.Unmarshal(file, &hosts)
+
+	rootCmd.Flags().StringVarP(&name, "name", "n", "", "主机名,多个用,分隔")
+	rootCmd.Flags().StringVarP(&command, "command", "c", "", "命令")
+	rootCmd.Flags().BoolVarP(&parallel, "parallel", "p", true, "是否并行")
+	rootCmd.Flags().IntVarP(&retry, "retry", "r", 0, "测试次数")
+	rootCmd.MarkFlagsRequiredTogether("name", "command")
+}
+
+func NewRuntime() connector.Runtime {
+	base := connector.NewBaseRuntime("run", connector.NewDialer(), false, false)
+	for _, h := range hosts {
+		h := h
+		base.AppendHost(&h)
+	}
+	return &base
 }
